f0ffbebeb2ee4754e4147f5f06ca3646931d21de
Isauro <isauro@avila-ti.com>
2024-10-24 17:08:10 -0600

    update reload memory usage script

--- vps/reload_memory_usage.sh
+++ vps/reload_memory_usage.sh
@@ -15,34 +15,25 @@ fi
 
 
 function get_free_memory_percentage() {
-    total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-    total_ram_gb=$(awk "BEGIN {printf \"%.2f\", $total_ram_kb / 1024 / 1024}")
 
-    used_ram_gb=$(free | grep Mem | awk '{print $3 / 1024 / 1024}')
+    total_ram_gb=$(free -h --si | grep Mem | awk '{print $2}' | sed 's/G//')
+    available_ram_gb=$(free -h --si | grep Mem | awk '{print $7}' | sed 's/G//')
 
-    free_ram_percentage=$(awk "BEGIN {if ($total_ram_gb > 0) printf \"%.2f\", ($total_ram_gb - $used_ram_gb) / $total_ram_gb * 100; else print \"0\"}")
+    free_ram_percentage=$(awk "BEGIN {if ($total_ram_gb > 0) printf \"%.2f\", $available_ram_gb / $total_ram_gb * 100; else print \"0\"}")
 
     echo "$free_ram_percentage"
+
 }
 
 function get_free_swap_percentage() {
-    total_swap_gb=$(free | grep Swap | awk '{print $2 / 1024 / 1024}')
-    used_swap_gb=$(free | grep Swap | awk '{print $3 / 1024 / 1024}')
 
-    # Manage the case where there is no swap configured
-    if (( $(echo "$total_swap_gb > 0" | bc -l) )); then
-        free_swap_percentage=$(awk "BEGIN {printf \"%.2f\", ($total_swap_gb - $used_swap_gb) / $total_swap_gb * 100}")
-    else
-        free_swap_percentage="0"
-    fi
+    total_swap_gb=$(free -h --si | grep Swap | awk '{print $2}' | sed 's/G//')
+    used_swap_gb=$(free -h --si | grep Swap | awk '{print $3}' | sed 's/G//')
+
+    free_swap_percentage=$(awk "BEGIN {if ($total_swap_gb > 0) printf \"%.2f\", ($total_swap_gb - $used_swap_gb) / $total_swap_gb * 100; else print \"0\"}")
 
     echo "$free_swap_percentage"
-}
 
-function restart_swap() {
-
-        swapoff /swapfile
-        swapon /swapfile
 }
 
 
@@ -63,30 +54,40 @@ fi
 
 # If the free swap percentage is less than 50% and the free RAM percentage is less than 10%, restart Apache and clear the swap
 if (( $(awk "BEGIN {print ($free_swap_percentage < 50)}") )) && (( $(awk "BEGIN {print ($free_ram_percentage < 10)}") )); then
-    echo "$(date): [ Reload Apache ] Free Swap Percentage: $free_swap_percentage%, Free RAM Percentage: $free_ram_percentage%" >> /var/log/reload_memory_usage.log
+
+    echo "$(date): [ Reloading Apache ] Free Swap Percentage: $free_swap_percentage%, Free RAM Percentage: $free_ram_percentage%" >> /var/log/reload_memory_usage.log
     systemctl restart apache2
+    sleep 60
 
-    sleep 120
-    new_free_ram_percentage=$(get_free_memory_percentage)
-
-    # If after restarting Apache the RAM free percentage is greater than 10%, restart the swap
-    if (( $(awk "BEGIN {print ($new_free_ram_percentage > 10)}") )); then
-        echo "$(date): [ Restart Swap ] Free RAM Percentage: $new_free_ram_percentage%" >> /var/log/reload_memory_usage.log
-        restart_swap
-    fi
 fi
 
-# check if the free swap percentage is less than 50%
-if (( $(awk "BEGIN {print ($free_swap_percentage < 50)}") )); then
-    echo "$(date): [ Free Swap Percentage ] Free Swap Percentage: $free_swap_percentage%" >> /var/log/reload_memory_usage.log
-    restart_swap
+# Only with the free RAM percentage less than 10%, restart the swap
+if (( $(awk "BEGIN {print ($free_ram_percentage < 10)}") )); then
+    echo "$(date): [ Restarting Apache ] Free RAM Percentage: $free_ram_percentage%" >> /var/log/reload_memory_usage.log
+    systemctl restart apache2
+    sleep 60
 fi
 
 
-echo "Free RAM Percentage: $free_ram_percentage%, Free Swap Percentage: $free_swap_percentage%"
+mysql_installed=$(dpkg -l | grep mysql-server)
 
-# Check if MySQL is running, if not, restart it
-if ! systemctl is-active --quiet mysql; then
-    echo "$(date): [ Restart MySQL ] MySQL is not running" >> /var/log/reload_memory_usage.log
-    systemctl start mysql
+# If MySQL is installed, try to start it 5 times if it is not running
+if [[ -n "$mysql_installed" ]]; then
+
+    for i in {1..5}; do
+        if systemctl is-active --quiet mysql; then
+            echo "$(date): [ MySQL ] MySQL is running" >> /var/log/reload_memory_usage.log
+            break
+        fi
+
+        echo "$(date): [ Start MySQL ] [try $i] MySQL is not running, trying to start it" >> /var/log/reload_memory_usage.log
+
+        systemctl start mysql
+        sleep 10
+    done
+
+    echo "$(date): [ MySQL ] $(systemctl is-active --quiet mysql && echo 'running' || echo 'not running')"
 fi
+
+echo "$(date): [ $free_ram_percentage%, $free_swap_percentage% ]"
+# echo mysql status
