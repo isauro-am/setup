741bf661b672737011d53a1139a259144a96f373
Isauro Avila <isauro@avila-ti.com>
2024-10-23 21:43:04 -0600

    update descriptions

--- vps/reload_memory_usage.sh
+++ vps/reload_memory_usage.sh
@@ -33,7 +33,7 @@ function get_free_swap_percentage() {
     total_swap_gb=$(free | grep Swap | awk '{print $2 / 1024 / 1024}')
     used_swap_gb=$(free | grep Swap | awk '{print $3 / 1024 / 1024}')
 
-    # Manejar el caso en que no haya swap configurado
+    # Manage the case where there is no swap configured
     if (( $(echo "$total_swap_gb > 0" | bc -l) )); then
         free_swap_percentage=$(awk "BEGIN {printf \"%.2f\", ($total_swap_gb - $used_swap_gb) / $total_swap_gb * 100}")
     else
@@ -43,22 +43,22 @@ function get_free_swap_percentage() {
     echo "$free_swap_percentage"
 }
 
-# Capturar el porcentaje de memoria libre y swap libre
+# Capture the free memory and swap percentages
 free_ram_percentage=$(get_free_memory_percentage)
 free_swap_percentage=$(get_free_swap_percentage)
 
-# Verificar si las variables son numéricas antes de continuar
+# Verify if the variables are numeric before continuing
 if ! [[ "$free_ram_percentage" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "Error: free_ram_percentage no es un número válido."
+    echo "Error: free_ram_percentage is not a valid number."
     exit 1
 fi
 
 if ! [[ "$free_swap_percentage" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "Error: free_swap_percentage no es un número válido."
+    echo "Error: free_swap_percentage is not a valid number."
     exit 1
 fi
 
-# Si el porcentaje de swap libre es menor al 50% y el de RAM libre es menor al 10%, reinicia Apache y limpia el swap
+# If the free swap percentage is less than 50% and the free RAM percentage is less than 10%, restart Apache and clear the swap
 if (( $(awk "BEGIN {print ($free_swap_percentage < 50)}") )) && (( $(awk "BEGIN {print ($free_ram_percentage < 10)}") )); then
     echo "$(date): [ Reload Apache ] Free Swap Percentage: $free_swap_percentage%, Free RAM Percentage: $free_ram_percentage%" >> /var/log/reload_memory_usage.log
     systemctl restart apache2
@@ -66,7 +66,7 @@ if (( $(awk "BEGIN {print ($free_swap_percentage < 50)}") )) && (( $(awk "BEGIN 
     sleep 120
     new_free_ram_percentage=$(get_free_memory_percentage)
 
-    # Si después de reiniciar Apache la RAM libre es mayor al 10%, reiniciar el swap
+    # If after restarting Apache the RAM free percentage is greater than 10%, restart the swap
     if (( $(awk "BEGIN {print ($new_free_ram_percentage > 10)}") )); then
         echo "$(date): [ Restart Swap ] Free RAM Percentage: $new_free_ram_percentage%" >> /var/log/reload_memory_usage.log
         swapoff -a
@@ -76,7 +76,7 @@ fi
 
 echo "Free RAM Percentage: $free_ram_percentage%, Free Swap Percentage: $free_swap_percentage%"
 
-# Verificar si MySQL está activo, si no, reiniciar
+# Check if MySQL is running, if not, restart it
 if ! systemctl is-active --quiet mysql; then
     echo "$(date): [ Restart MySQL ] MySQL is not running" >> /var/log/reload_memory_usage.log
     systemctl start mysql
